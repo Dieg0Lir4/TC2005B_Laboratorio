adf90e1bbd48d0b4dab05ac27e98eac29db199d1
Diego Lira <diegolira@MacBook-Air-de-Diego-LG.local>
2024-03-11 12:06:58 -0600

    Lab17 pausado para hacer el 18

--- Laboratorio13_en_adelante/controllers/users.controller.js
+++ Laboratorio13_en_adelante/controllers/users.controller.js
@@ -9,3 +9,8 @@ exports.post_login = (req, res) => {
     res.redirect('/profesor');
 }
 
+exports.get_logout = (request, response, next) => {
+    request.session.destroy(() => {
+        response.redirect('/perfil/login'); //Este código se ejecuta cuando la sesión se elimina.
+    });
+}
